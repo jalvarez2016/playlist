47bea567306c8f3f50c171b84e0df53e19f65190
Jason <alvarezcoraiza@citypolyhs.org>
2018-02-27 23:13:58 +0000

    fix delete button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // quick fix for Cloud9 warning:
 /* global $ */
-
+/* global localStorage*/
+/* global Storage*/
 // Class 2:
 // Complete displayList() to show a single song in the list
 
@@ -16,14 +17,13 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 // Books
-var mybook = {
-	"title":"Ready Player One",
-	"author":"Earnest Cline",
-	"url":"https://books.google.com/books/about/Ready_Player_One.html?id=TQtFDwAAQBAJ&printsec=frontcover&source=kp_read_button#v=onepage&q&f=false",
-	"imageurl":"https://images-na.ssl-images-amazon.com/images/I/61d6DhRCBSL._SX322_BO1,204,203,200_.jpg",
-}
-
 var myToReadList = [
+	{
+	    "title":"Ready Player One",
+	    "author":"Earnest Cline",
+	    "url":"https://books.google.com/books/about/Ready_Player_One.html?id=TQtFDwAAQBAJ&printsec=frontcover&source=kp_read_button#v=onepage&q&f=false",
+	    "imageurl":"https://images-na.ssl-images-amazon.com/images/I/61d6DhRCBSL._SX322_BO1,204,203,200_.jpg",
+    },
 	{
 		"title":"The Amazing Adventures of Kavalier and Clay",
 		"author":"Michael Chabon",
@@ -49,12 +49,11 @@ var myToReadList = [
 		"imageurl":"https://i0.wp.com/www.kevinrandom.com/wp-content/uploads/2013/06/talent-code-cover.jpg?resize=225%2C300",
 	}
 ];
-//Issuu is a free reading app
+
 
 
 // DOCUMENT READY FUNCTION
 $( document ).ready(function() {
-  console.log(mybook.title);
   displayList();
   $("#save").click(function(){
  	addBook();
@@ -66,45 +65,49 @@ $( document ).ready(function() {
 });
 
 function displayList(){
- $(".books").append("<div id='delete'>" + "<button class='btn btn-primary' onclick='myFunction()'>Delete</button>" + "<p>"+ mybook.title +"</p>" + "<p> By : "+ mybook.author +"</p>" + "<img height='300px' width='200px' src='"+ mybook.imageurl +"'>" + "<p><a href='"+ mybook.url +"'>Start Reading</p>" + "<hr style='border-color: #ffffff'>" + "</div>");
+ $(".books").empty();
  for(var i= 0; i < myToReadList.length ; i++){
 	$(".books").append("<div id='"+ i +"'>" + "<button class='btn btn-primary' onclick='singleDelete(" + i + ")'>Delete</button>" + "<p>" + myToReadList[i].title + "</p>" + "<p> By : " + myToReadList[i].author + "</p>" + "<img height='300px' width='200px' src='" + myToReadList[i].imageurl + "'>" + "<p><a href='" + myToReadList[i].url + "'>Start Reading</p>" + "<hr style='border-color: #ffffff' border-width='10px'>" + "</div>");
 	console.log(myToReadList[i].url);
  }
 }
-
+var counter = 0;
 function singleDelete(i){
-  
   var x = document.getElementById(i);
   if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
+        console.log(myToReadList[i]);
+        console.log(myToReadList);
     }
+  if (i === counter) {
+     myToReadList.splice(i,1);
+     console.log(myToReadList[i]);
+     console.log(myToReadList);
+  } else {
+      
+  }
+  counter++;
 }
 
 function clearList(){
   
   $(".books").empty();
-  
-}
-
-function myFunction(){
-  
-  var x = document.getElementById("delete");
-  if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+  myToReadList.splice(0);
+  displayList();
   
 }
 
 function addBook(){
-	var title = $("#title").val();
-	var author = $("#author").val();
-	var url = $("#url").val();
+	var name = $("#title").val();
+	var person = $("#author").val();
+	var place = $("#url").val();
 	var img = $("#image").val();
-	
-	$(".books").append("<div id='delete'>" + "<button class='btn btn-primary' onclick='myFunction()'>Delete</button>" + "<p>" + title + "</p>" + "<p> By : " + author + "</p>" + "<img height='300px' width='200px' src='" + url + "'>" + "<p><a href='" + img + "'>Start Reading</p>" + "<hr style=' border-color: #ffffff' borer-width='10px'>" + "</div>");
+	//document.getElementByClass("books").innerHTML = localStorage.list;
+	//$(".books").append("<div id='delete'>" + "<button class='btn btn-primary' onclick='myFunction()'>Delete</button>" + "<p>" + title + "</p>" + "<p> By : " + author + "</p>" + "<img height='300px' width='200px' src='" + url + "'>" + "<p><a href='" + img + "'>Start Reading</p>" + "<hr style=' border-color: #ffffff' borer-width='10px'>" + "</div>");
+	myToReadList.push({ title : name , author : person , url : place , imageurl : img ,});
+	console.log(myToReadList);
+	localStorage.list = myToReadList;
+	displayList();
 }
